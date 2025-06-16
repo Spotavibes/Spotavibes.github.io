d948b4d9d8d43652f40531b04ab5ddf97ccbf0f5
Spotavibes <spotavibes@gmail.com>
2025-06-15 20:24:48 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Spotavibes.github.io/', // 👈 Set to your repo name
+  base: '/Spotavibes.github.io/', // 🔁 Replace with your repo name
   plugins: [react()],
 });
